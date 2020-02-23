112c1b7d5d070dd29fc70a4316a87170af67b16f
Dai Kawahara <skbsporta7@gmail.com>
2020-02-23 12:28:32 +0900

    Initial commit

---
+++ Multiple-Module-Memo-Sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Multiple-Module-Memo-Sample
+//
+//  Created by kawaharadai on 2020/02/23.
+//  Copyright © 2020 kawaharadai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
