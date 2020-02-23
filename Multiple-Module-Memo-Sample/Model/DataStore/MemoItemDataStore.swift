d8bce5fce3ac8af716c897d2e92c1251237d3792
Dai Kawahara <skbsporta7@gmail.com>
2020-02-23 13:35:41 +0900

    add model layer interface

--- Multiple-Module-Memo-Sample/Model/DataStore/MemoItemDataStore.swift
+++ Multiple-Module-Memo-Sample/Model/DataStore/MemoItemDataStore.swift
@@ -7,7 +7,21 @@
 //
 
 import Foundation
+import CoreData
 
 protocol MemoItemDataStore {
-    
+    func create<T: NSManagedObject>(entityName: String, _ completion: (Result<T, Error>) -> (Void))
+
+    @discardableResult
+    func save(context: NSManagedObjectContext) -> Result<Void, Error>
+
+    func fetchArray<T: NSManagedObject>(predicates: [NSPredicate],
+                                        sortKey: String,
+                                        ascending: Bool,
+                                        logicalType: NSCompoundPredicate.LogicalType,
+                                        _ completion: (Result<[T], Error>) -> (Void))
+
+    func excute<R: NSPersistentStoreRequest>(request: R, _ completion: (Void) -> (Void))
+
+    func delete<T: NSManagedObject>(object: T, _ completion: (Void) -> (Void))
 }
--- Multiple-Module-Memo-Sample/Model/Repository/MemoItemRepository.swift
+++ Multiple-Module-Memo-Sample/Model/Repository/MemoItemRepository.swift
@@ -6,5 +6,17 @@
 import Foundation
 
 protocol MemoItemRepository {
-    
+    func createMemoItem(text: String, uniqueId: String?, _ completion: (Result<Memo, Error>) -> (Void))
+
+    func readAllMemos(_ completion: (Result<[Memo], Error>) -> Void)
+
+    func readMemo(at uniqueId: String, _ completion: (Result<Memo, Error>) -> (Void))
+
+    func updateMemo(_ memo: Memo, text: String, _ completion: (Void) -> (Void))
+
+    func deleteAllMemos(entityName: String, _ completion: (Void) -> (Void))
+
+    func deleteMemo(at uniqueId: String, _ completion: (Void) -> (Void))
+
+    func countAllMemos(_ completion: (Void) -> (Void))
 }
