58993f52e7e57e9aa949b7f026c0e7b575d45ff6
Dai Kawahara <skbsporta7@gmail.com>
2020-02-29 19:06:10 +0900

    add MemoInfo

---
+++ Multiple-Module-Memo-Sample/ValueObject/MemoInfo.swift
@@ -0,0 +1,23 @@
+//
+//  MemoInfo.swift
+//  Multiple-Module-Memo-Sample
+//
+//  Created by kawaharadai on 2020/02/29.
+//  Copyright © 2020 kawaharadai. All rights reserved.
+//
+
+import Foundation
+
+struct MemoInfo {
+    let uniqueId: String
+    let title: String
+    let content: String
+    let editDate: Date?
+
+    init(uniqueId: String?, title: String?, content: String?, editDate: Date?) {
+        self.uniqueId = uniqueId ?? ""
+        self.title = title ?? ""
+        self.content = content ?? ""
+        self.editDate = editDate
+    }
+}
