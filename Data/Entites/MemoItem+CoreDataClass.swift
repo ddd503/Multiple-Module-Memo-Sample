ae9d704087a2d2b6798e9c507da649771dfbad9b
Dai Kawahara <skbsporta7@gmail.com>
2020-02-29 19:26:07 +0900

    add MemoItem

---
+++ Data/Entites/MemoItem+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  MemoItem+CoreDataClass.swift
+//  Data
+//
+//  Created by kawaharadai on 2020/02/29.
+//  Copyright © 2020 kawaharadai. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(MemoItem)
+public class MemoItem: NSManagedObject {
+
+}
